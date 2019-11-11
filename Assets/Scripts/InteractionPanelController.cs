036dcecdace8240a9b74c0073552364aaa3cefd9
Teemu Salminen <teemujsa@gmail.com>
2019-11-11 12:31:53 +0200

    Initial commit

---
+++ Assets/Scripts/InteractionPanelController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class InteractionPanelController : MonoBehaviour
+{
+    public ParticleSystem Particles;
+
+    public Button ResetButton;
+    public InputField SpeedField;
+    public Toggle ParticlesActiveToggle;
+    public Slider OpacitySlider;
+
+    void Start()
+    {
+        ResetButton.onClick.AddListener(() => {
+            Particles.Clear();
+        });
+
+        SpeedField.onValueChanged.AddListener((value) =>
+        {
+            try
+            {
+                float speed = float.Parse(value);
+                Particles.startSpeed = speed;
+            }
+            catch(Exception e) { }
+        });
+
+        ParticlesActiveToggle.onValueChanged.AddListener((active) => {
+            Particles.gameObject.SetActive(active);
+        });
+
+        Particles.startColor = new Color(1, 1, 1, OpacitySlider.value);
+        OpacitySlider.onValueChanged.AddListener((value) =>
+        {
+            Particles.startColor = new Color(1, 1, 1, value);
+        });
+    }
+}
